a378a5abad50416ba962e354fd9a7ec3581b5096
|| Prof. - Xadk3!#0000 || @naryal2580 <naryal2580@proton.me>
2023-06-18 20:32:20 +0545

    read 3rd party clients json

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
 const tools = require("./nostr-tools");
+const fs = require("fs");
 
 console.log(tools)
+
+const third_party_clients = JSON.parse(fs.readFileSync("third-party-clients.json").toString());
+
+console.log(third_party_clients)
 
 // relay = tools.relayInit();
